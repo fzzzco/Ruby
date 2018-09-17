2e9fa378538f80ad80b90742b6433b73218d69e7
fzzzco <brandl.s@gmx.net>
2018-09-17 22:40:20 +0200

    lessons 1 - 3

---
+++ learning/ex2.rb
@@ -0,0 +1,9 @@
+#A commt, this is so you can read your program later.
+#Anythin after the # is ignorde by ruby
+
+puts "I could have code like this" # and a comment after is ignored
+
+# You can also use a comment to "disable" or comment after is ignored
+# puts "This won't run."
+
+puts "This will run."
