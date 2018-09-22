c50217d354629faf70b2fe74f6c7eff3962dfc5d
fzzzco <brandl.s@gmx.net>
2018-09-22 18:00:11 +0200

    exercise 7 - 16

---
+++ learning/ex12a.rb
@@ -0,0 +1,6 @@
+print "Give my some money and i will show you a trick: "
+money = gets.chomp.to_f
+
+returnmoney = money * 0.1
+
+puts "I can make 90% of your money disappear.... Here is the rest: #{returnmoney}."
---
+++ learning/ex13a.rb
@@ -0,0 +1,4 @@
+test = ARGV
+puts "Insert anything:"
+test2 = $stdin.gets.chomp
+puts "the frist one is an argument: #{test} \n and the second one: #{test2}."
